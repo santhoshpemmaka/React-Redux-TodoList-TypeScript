29699d53fbccda1dddc65c7d642234259f059823
Santhosh_Pemmaka <santhosh.pemmaka@gmail.com>
2022-08-16 23:05:07 +0530

    changes

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -11,76 +11,71 @@ import {
   SHOW_COMPLETED
 } from "../containers/todoContainers";
 import { bindActionCreators } from "redux";
-import "./createTodo.scss";
-import { ITodoTableProps } from "../interface/createtodo";
+import "./createTodo.css";
+import {ITodoTableProps} from "../interface/createtodo";
 
 class Table extends Component<ITodoTableProps, any> {
-  render() {
-    return (
-      <div className="todos">
-        <div className="todoHeader">
-          <span
-            className="todoShow"
-            onClick={() => this.props.setVisibilityFilter(SHOW_ALL)}
-          >
-            All
-          </span>
-          <span
-            className="todoShow"
-            onClick={() => this.props.setVisibilityFilter(SHOW_COMPLETED)}
-          >
-            Completed
-          </span>
-          <span
-            className="todoShow"
-            onClick={() => this.props.setVisibilityFilter(SHOW_ACTIVE)}
-          >
-            Active
-          </span>
-        </div>
-        <div>
-          {this.props.todos && this.props.todos.length > 0 ? (
-            <div className="todoTable">
-              <div className="tableHeader">
-                <span className="todohead">Todos</span>
-                <span className="todohead1">Actions</span>
-              </div>
-              {this.props.todos &&
-                this.props.todos.map(todo => {
-                  return (
-                    <div key={todo.id} className="todolist">
-                      <span
-                        style={{
-                          textDecoration: todo.completed
-                            ? "line-through"
-                            : "none"
-                        }}
-                      >
-                        {todo.text}
-                      </span>
-                      <span className="todobtns">
-                        <button
-                          onClick={() => this.props.deleteTodo(todo.id)}
-                          className="todobtn"
-                        >
-                          Remove
-                        </button>
-                        <button
-                          onClick={() => this.props.toggleTodo(todo.id)}
-                          className="todobtn"
-                        >
-                          Completed
-                        </button>
-                      </span>
-                    </div>
-                  );
-                })}
-            </div>
-          ) : null}
-        </div>
-      </div>
-    );
-  }
+	props: any;
+	render() {
+		return (
+			<div className='todos'>
+				<div className='todoHeader'>
+					<span
+						className='todoShow'
+						onClick={() => this.props.setVisibilityFilter(SHOW_ALL)}>
+						All
+					</span>
+					<span
+						className='todoShow'
+						onClick={() => this.props.setVisibilityFilter(SHOW_COMPLETED)}>
+						Completed
+					</span>
+					<span
+						className='todoShow'
+						onClick={() => this.props.setVisibilityFilter(SHOW_ACTIVE)}>
+						Active
+					</span>
+				</div>
+				<div>
+					{this.props.todos && this.props.todos.length > 0 ? (
+						<div className='todoTable'>
+							<div className='tableHeader'>
+								<span className='todohead'>Todos</span>
+								<span className='todohead1'>Actions</span>
+							</div>
+							{this.props.todos &&
+								this.props.todos.map((todo) => {
+									return (
+										<div key={todo.id} className='todolist'>
+											<span
+												style={{
+													textDecoration: todo.completed
+														? "line-through"
+														: "none",
+												}}>
+												{todo.text}
+											</span>
+											<span className='todobtns'>
+												<button
+													onClick={() => this.props.deleteTodo(todo.id)}
+													className='todobtn'>
+													Remove
+												</button>
+												<button
+													onClick={() => this.props.toggleTodo(todo.id)}
+													className='todobtn'>
+													Completed
+												</button>
+											</span>
+										</div>
+									);
+								})}
+						</div>
+					) : null}
+				</div>
+			</div>
+		);
+	}
 }
 
 const getvisibleTodos = (todos, filter) => {
