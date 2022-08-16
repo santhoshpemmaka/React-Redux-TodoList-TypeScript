d825277b73c75f4845f562b49795c96b111b63cf
Santhosh_Pemmaka <santhosh.pemmaka@gmail.com>
2022-08-16 23:03:12 +0530

    changes

--- src/components/createtodoList.tsx
+++ src/components/createtodoList.tsx
@@ -2,52 +2,55 @@ import React, { Component } from "react";
 import { addTodo } from "../actions/todoAction";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import "./createTodo.scss";
-import { ITodoProps } from "../interface/createtodo";
+import "./createTodo.css";
+import {ITodoProps} from "../interface/createtodo";
 
 class CreateTodo extends Component<ITodoProps, any> {
-  constructor(props: ITodoProps) {
-    super(props);
-    this.state = {
-      todotext: ""
-    };
-  }
-  onChangeTodotext = e => {
-    let text = e.target.value;
-    this.setState({
-      todotext: text
-    });
-  };
-  render() {
-    return (
-      <div className="header">
-        <input
-          className="createTodo"
-          type="text"
-          value={this.state.todotext}
-          onChange={this.onChangeTodotext}
-          placeholder="add todo here"
-        />
-        <button
-          className="btnCancel"
-          type="button"
-          onClick={() => this.setState({ todotext: "" })}
-        >
-          Cancel
-        </button>
-        <button
-          className="btnAdd"
-          type="button"
-          onClick={() => {
-            this.props.addTodo(this.state.todotext);
-            this.setState({ todotext: "" });
-          }}
-        >
-          Add Todo
-        </button>
-      </div>
-    );
-  }
+	state: {todotext: string};
+	setState(arg0: {todotext: any}) {
+		throw new Error("Method not implemented.");
+	}
+	props: any;
+	constructor(props: ITodoProps) {
+		super(props);
+		this.state = {
+			todotext: "",
+		};
+	}
+	onChangeTodotext = (e) => {
+		let text = e.target.value;
+		this.setState({
+			todotext: text,
+		});
+	};
+	render() {
+		return (
+			<div className='header'>
+				<input
+					className='createTodo'
+					type='text'
+					value={this.state.todotext}
+					onChange={this.onChangeTodotext}
+					placeholder='add todo here'
+				/>
+				<button
+					className='btnCancel'
+					type='button'
+					onClick={() => this.setState({todotext: ""})}>
+					Cancel
+				</button>
+				<button
+					className='btnAdd'
+					type='button'
+					onClick={() => {
+						this.props.addTodo(this.state.todotext);
+						this.setState({todotext: ""});
+					}}>
+					Add Todo
+				</button>
+			</div>
+		);
+	}
 }
 
 const mapDistachProps = dispatch => {
